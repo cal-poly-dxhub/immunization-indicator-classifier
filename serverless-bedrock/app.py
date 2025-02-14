9dcced0e664721b0a9f37accbb6c0d452ad91073
Belal Elshenety <belal.elshenety@gmail.com>
2025-02-13 22:54:39 -0800

    Fixed snomed-to-cdsi lambda to return description of snomed and title of cdsi and made a separate folder for each lambda

--- serverless-bedrock/app.py
+++ serverless-bedrock/app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 import os
 import aws_cdk as cdk
-from serverless_bedrock_stack import ServerlessBedrockStack
+from stacks.serverless_bedrock_stack import ServerlessBedrockStack
+from stacks.SNOMED_to_CDSi_stack import ServerlessSNOMEDTOCDSi
 
 app = cdk.App()
 ServerlessBedrockStack(app, "ServerlessBedrockStack")
+ServerlessSNOMEDTOCDSi(app, "ServerlessSNOMEDTOCDSi")
 app.synth()
